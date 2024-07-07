87a7bd8e1140d4d5db521a452fba03ea84ae821a
ankithshetty17 <ankishetty2002@gmail.com>
2024-07-07 20:12:38 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div className=' justify-center items-center text-center'>
+        <h1 className='text-blue-500 '>Hello</h1>
+      </div>
+    
+  )
+}
+
+export default App
